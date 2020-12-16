bde5d077a8acdb0fd7c1a8b64b231f02f90f0be8
abishekk92 <bhat.abishek@gmail.com>
2020-12-16 15:38:51 +0530

    Add symlinks

---
+++ configure_links.fish
@@ -0,0 +1,6 @@
+ln -sv ~/code/abishekk92/dotfiles/alacritty/alacritty.yml alacritty/alacritty.yml
+ln -sv ~/code/abishekk92/dotfiles/fish/config.fish fish/config.fish
+ln -sv ~/code/abishekk92/dotfiles/nvim/init.vim nvim/init.vim
+ln -sv ~/code/abishekk92/dotfiles/git/.gitconfig ~
+ln -sv ~/code/abishekk92/dotfiles/git/.gitignore ~
+ln -sv ~/code/abishekk92/dotfiles/tmux/.tmux.conf ~
