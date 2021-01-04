b4e8c8b8fc347ed81c9b42354a8733823c9ca2b0
abishekk92 <bhat.abishek@gmail.com>
2021-01-04 10:49:58 +0530

    Fix autojump path

--- fish/config.fish
+++ fish/config.fish
@@ -9,12 +9,7 @@ alias vim="nvim"
 alias ls="lsd"
 alias hgrep="history | rg"
 
-begin
-    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
-    if test -e $AUTOJUMP_PATH
-        source $AUTOJUMP_PATH
-    end
-end
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 set theme_complete_path "yes"
 
